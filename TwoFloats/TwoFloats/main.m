938195bf93c43232f8a16b2862255146fa9f1c3b
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-30 18:26:30 +0100

    First ObjC project

---
+++ TwoFloats/TwoFloats/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TwoFloats
+//
+//  Created by Jacob on 30/04/2015.
+//  Copyright (c) 2015 Bravery Communications. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    float jake;
+    float dave;
+    jake = 10.2;
+    dave = 10.2;
+    
+    double time;
+    
+    time = jake * dave;
+    
+    printf("That's %f", time);
+}
