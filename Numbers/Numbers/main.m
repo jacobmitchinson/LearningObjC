dd942f418ca1cad43b1f58e14d7ec73e097b58fa
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:10:25 +0100

    Numbers

--- Numbers/Numbers/main.m
+++ Numbers/Numbers/main.m
@@ -7,11 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    int x = 255;
+    printf("x is %d.\n", x);
+    printf("In octal, x is %o.\n", x);
+    printf("In hexadecimal, x is %x\n", x);
+    
+    long z = 255;
+    printf("This is a long %ld\n", z);
+    
+    unsigned long o = 255;
+    printf("x is %lu.\n", o);
+    
+    printf("The absolute of value of -5 is %d\n", abs(-5));
+    
+    double y = 123545.67890;
+    printf("y is %f", y);
+    printf("y is %e", y);
     return 0;
 }
