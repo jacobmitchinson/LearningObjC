f3b1b351e64bfb529bbf50035115a90fa66aa75f
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:09:03 +0100

    Calculating float size

--- FloatSize/FloatSize/main.m
+++ FloatSize/FloatSize/main.m
@@ -7,11 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <readline/readline.h>
+
+void calculateSizeOfShort(short i) {
+    short *number = &i;
+    printf("%zu", sizeof(number));
+}
+
+void calculateSizeOfPointer(float number) {
+    float *pointer = &number;
+    printf("%zu", sizeof(pointer));
+}
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    const char *numberString = readline(NULL);
+    float number = atof(numberString);
+    calculateSizeOfPointer(number);
+    short i = 2;
+    calculateSizeOfShort(i);
     return 0;
 }
