92947c640254f6d81905affa1a8e92bc7009f41f
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:09:26 +0100

    Buzz challenge

--- FuzzyBuzzy/FuzzyBuzzy/main.m
+++ FuzzyBuzzy/FuzzyBuzzy/main.m
@@ -6,12 +6,25 @@
 //  Copyright (c) 2015 Bravery Communications. All rights reserved.
 //
 
+#import <readline/readline.h>
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+void isDivisibleByFive(int number) {
+    if(number % 5 == 0) {
+        printf("Found one!\n");
+    };
+}
+
+void count(int total) {
+    for(int i = total; i > 0; i -= 3) {
+        printf("%d\n", i);
+        isDivisibleByFive(i);
     }
+}
+
+int main(int argc, const char * argv[]) {
+    const char *countTo = readline(NULL);
+    int number = atoi(countTo);
+    count(number);
     return 0;
 }
