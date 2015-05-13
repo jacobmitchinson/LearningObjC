adc039e800dac83854392df3beee4b7c77cf3213
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:10:06 +0100

    Advanced handling of memory

--- MemoryBMI/MemoryBMI/main.m
+++ MemoryBMI/MemoryBMI/main.m
@@ -7,11 +7,29 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float heightInMeters;
+    int weightInKilos;
+} Person;
+
+float bodyMassIndex(Person *p) {
+    return p->weightInKilos / (p->heightInMeters * p->heightInMeters);
+}
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
+    Person *mikey = (Person *)malloc(sizeof(Person));
+    
+    mikey->weightInKilos = 96;
+    mikey->heightInMeters = 1.7;
+    
+    float mikeyBMI = bodyMassIndex(mikey);
+    printf("mikey has a BMI of %f\n", mikeyBMI);
+    
+    free(mikey);
+    mikey = NULL;
     return 0;
 }
