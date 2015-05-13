b109c7c8bc20e2202dae9d1d231ccc2139d5b481
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:10:16 +0100

    Pointers

--- MetersInFeetUsingModf/MetersInFeetUsingModf/main.m
+++ MetersInFeetUsingModf/MetersInFeetUsingModf/main.m
@@ -8,10 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr) {
+    double inches, feet;
+    double rawFeet = meters * 3.281;
+    inches = modf(rawFeet, &feet);
+    *ftPtr = feet;
+    *inPtr = inches;
+}
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    double meters = 3.0;
+    unsigned int feet;
+    double inches;
+    
+    metersToFeetAndInches(meters, &feet, &inches);
+    printf("%.1f meters is equal to %d feet and %.1f inches", meters, feet, inches);
     return 0;
 }
