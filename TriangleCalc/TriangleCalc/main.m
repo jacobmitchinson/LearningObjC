ac79b79de3e873023605d2962c8fccaa91e7a9ea
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:11:49 +0100

    Counting challenge

--- TriangleCalc/TriangleCalc/main.m
+++ TriangleCalc/TriangleCalc/main.m
@@ -8,10 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
+int remainingAngle(float angleA, float angleB) {
+    float total = 180.0;
+    float angleC = total - angleA - angleB;
+    return angleC;
+}
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    float angleA = 30.0;
+    float angleB = 60.0;
+    float angleC = remainingAngle(angleA, angleB);
+    printf("The third angle is %.2f\n", angleC);
     return 0;
 }
