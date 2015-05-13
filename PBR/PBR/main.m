c0b694b48d1a656d230377a42c8ae8da3bacf6f8
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:10:45 +0100

    Pass-By-Reference

--- PBR/PBR/main.m
+++ PBR/PBR/main.m
@@ -8,10 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr) {
+    double rawFeet = meters * 3.281;
+    unsigned int feet = (unsigned int)floor(rawFeet);
+    
+    if(ftPtr) {
+        *ftPtr = feet;
     }
+
+    double fractionalFoot = rawFeet - feet;
+    double inches = fractionalFoot * 12.0;
+    
+    if(inPtr) {
+        *inPtr = inches;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    double meters = 3.0;
+    unsigned int feet;
+    double inches;
+    
+    metersToFeetAndInches(meters, &feet, &inches);
+    printf("%.1f meters is equal to %d feet and %.1f inches.", meters, feet, inches);
     return 0;
 }
