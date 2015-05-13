d667de00b06d629f42044eb0f95bec44b5dba961
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-13 14:11:36 +0100

    Calculating time

--- TimeStruct/TimeStruct/main.m
+++ TimeStruct/TimeStruct/main.m
@@ -9,9 +9,11 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    long secondsSince1970 = time(NULL);
+    printf("It has been %ld seconds since 1970\n", secondsSince1970);
+    struct tm now;
+    long addFourMillionSeconds = 4000000 + secondsSince1970;
+    localtime_r(&addFourMillionSeconds, &now);
+    printf("The date will be %d:%d:%d\n", now.tm_mday, now.tm_mon + 1, 1900 + now.tm_year);
     return 0;
 }
