5adc56aeb3a6cc0c620adc1552140286405772cf
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-20 09:07:48 +0100

    Understanding arguments

--- Arguments/Arguments/main.m
+++ Arguments/Arguments/main.m
@@ -10,8 +10,28 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSDate *now = [NSDate date];
+        NSLog(@"This NSDate object lives at %p", now);
+        NSLog(@"The date is %@", now);
+        
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since of the start of 1970", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds the time will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        unsigned long day = [cal ordinalityOfUnit:NSDayCalendarUnit
+                                           inUnit:NSMonthCalendarUnit
+                                          forDate:now];
+        
+        double newSeconds = [[NSDate date] timeIntervalSince1970];
+        
+        NSLog(@"The current calendar is %@", [cal calendarIdentifier]);
+        
+        NSDate *newDate = [[NSDate alloc] init];
+    
     }
     return 0;
 }
